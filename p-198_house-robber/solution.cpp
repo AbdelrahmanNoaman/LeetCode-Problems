9435d952c5b7499f0b2a9dec1c4bb0de939e88ec
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2025-06-17 16:36:55 +0300

    Optimize: refactor p-198 code to reduce number of redundant if conditions

--- p-198_house-robber/solution.cpp
+++ p-198_house-robber/solution.cpp
@@ -6,17 +6,16 @@
 
 int Solution::solve(std::vector<int>nums){
     std::vector<int> bestChoices(nums.size(), 0);
+    int size = nums.size();
+    if(size>1){
+        bestChoices[size-1] = nums[size-1];
+    }
+    if(size>2){
+        bestChoices[size-2] = std::max(nums[size-2],bestChoices[size-1]);
+    }
 
-    for(int i=nums.size()-1;i>=0;i--){
-        if(i == nums.size()-1){
-            bestChoices[i] = nums[i];
-        }
-        else if (i == nums.size()-2){
-            bestChoices[i] = std::max(nums[i],bestChoices[i+1]);
-        }
-        else{
-            bestChoices[i] = std::max(nums[i]+bestChoices[i+2],bestChoices[i+1]);
-        }
+    for(int i=nums.size()-3;i>=0;i--){
+        bestChoices[i] = std::max(nums[i]+bestChoices[i+2],bestChoices[i+1]);
     }
 
     return bestChoices[0];
