e6f4c574f7f49240d4cd0f84fc8552b614f2d0a1
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2025-06-18 15:53:02 +0300

    Feat: Solve Problem 2966

---
+++ p-2966_divide_array_into_arrays_with_max_difference/solution.h
@@ -0,0 +1,15 @@
+#include<vector>
+
+#ifndef SOLUTION_2966
+#define SOLUTION 2966
+
+class Solution{
+    private:
+        std::vector<std::vector<int>> solve(std::vector<int>values,int k);
+    public:
+        std::vector<std::vector<int>> test(std::vector<int>values,int k);
+};
+
+
+
+#endif // SOLUTION_2966
