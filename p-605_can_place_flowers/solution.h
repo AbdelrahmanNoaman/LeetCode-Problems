e4174f6045f2482e28e2f5ef499edfd64f88dc5c
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2025-06-27 20:56:47 +0300

    Feat: Solve Problem 605

---
+++ p-605_can_place_flowers/solution.h
@@ -0,0 +1,15 @@
+#include<vector>
+
+#ifndef SOLUTION_605
+#define SOLUTION_605
+
+class Solution{
+
+    private:
+        bool solve(std::vector<int> flowerbed, int n);
+    public:
+        bool test(std::vector<int> flowerbed, int n);
+
+};
+
+#endif SOLUTION_605
