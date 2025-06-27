e2739b6d5834ebb05aaf6c8cf94f19c1e314b994
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2025-06-27 21:16:16 +0300

    Feat: Solve Problem 345

---
+++ p-345_reverse_vowels_of_a_string/solution.h
@@ -0,0 +1,17 @@
+#include<string>
+#include<vector>
+
+#ifndef SOLUTION_345
+#define SOLUTION_345
+
+class Solution{
+
+    private:
+        std::string solve(std::string s);
+        std::string twoPointersSol(std::string s);
+    public:
+        std::string test(std::string s);
+
+};
+
+#endif SOLUTION_345
