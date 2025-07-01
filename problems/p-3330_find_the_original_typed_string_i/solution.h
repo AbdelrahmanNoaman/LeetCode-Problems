dc8b8b0c9940fbe5c802c91118c4fb812338e480
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2025-07-01 13:52:02 +0300

    Feat: Solve Problem 3330

---
+++ problems/p-3330_find_the_original_typed_string_i/solution.h
@@ -0,0 +1,13 @@
+#include<string>
+
+#ifndef SOLUTION_3330
+#define SOLUTION_3330
+
+class Solution{
+    private:
+        int solve(std::string s);
+    public:
+        int test(std::string s);
+};
+
+#endif // SOLUTION_3330
