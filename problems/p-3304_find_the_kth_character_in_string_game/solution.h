e4916ba5679be939ca6680717b162290253723e9
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2025-07-03 15:34:23 +0300

    Feat : Solve Problem 3304

---
+++ problems/p-3304_find_the_kth_character_in_string_game/solution.h
@@ -0,0 +1,15 @@
+#include<vector>
+
+#ifndef SOLUTION_3304
+#define SOLUTION 3304
+
+class Solution{
+    private:
+        char solve(int k);
+    public:
+        char test(int k);
+};
+
+
+
+#endif // SOLUTION_3304
