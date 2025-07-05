34ec7430725ea027e85874559242652cb6b6c7d5
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2025-07-05 21:44:50 +0300

    Feat: Solve Problem 735

---
+++ problems/p-735_asteroid_collision/solution.h
@@ -0,0 +1,15 @@
+#include<vector>
+
+#ifndef SOLUTION_735
+#define SOLUTION_735
+
+class Solution{
+
+    private:
+        std::vector<int> solve(std::vector<int> nums);
+    public:
+        std::vector<int> test(std::vector<int> nums);
+
+};
+
+#endif SOLUTION_735
