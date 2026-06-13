1a4838b39747359cfa0f0e37299ec2a7c35b41e2
AbdelrahmanNoaman <bodynoaman1996@gmail.com>
2026-06-13 16:25:19 +0300

    feat: Solve 17. Letter Combinations of a Phone Number

---
+++ problems/p-17_letter_combinations_of_a_phone_number/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "solution.h"
+
+int main(){
+    std::string digits = "23";
+    
+    Solution LetterCombinationsProblem;
+    LetterCombinationsProblem.test(digits);
+    
+    return 0;
+}
---
+++ problems/p-17_letter_combinations_of_a_phone_number/solution.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+#ifndef SOLUTION_17
+#define SOLUTION_17
+
+class Solution{
+
+    private:
+        std::vector<std::string> solve(std::string digits);
+    public:
+        void test(std::string digits);
+
+};
+
+#endif SOLUTION_17
